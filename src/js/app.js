36d6e4564ab9ecb382e59ebe2269ea478e12c526
Vladislav Osipenko <vladislavosipenko584@gmail.com>
2023-07-06 21:46:47 +0300

    Done "main" section with adaptation and start section "house"

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,22 @@ flsFunctions.isWebp();
 
 import Swiper, { Navigation, Pagination } from "swiper";
 
-const swiper = new Swiper('.swiper', {
-    
+document.addEventListener('DOMContentLoaded', () => {
+    const swiper = new Swiper('.main__swiper', {
+        slidesPerView: 2,
+        breakpoints: {
+            1024: {
+                slidesPerView: 2,
+            },
+            756: {
+                slidesPerView: 3,
+            },
+
+            320: {
+                slidesPerView: 1,
+            }
+            
+        },
+    });
 });
+
