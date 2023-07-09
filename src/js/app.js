b931ab21747c0c1c575bc6021f915b3a84448f58
Vladislav Osipenko <vladislavosipenko584@gmail.com>
2023-07-09 16:13:19 +0300

    Done thre sections with adaptation

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import * as flsFunctions from "./modules/functions.js";
+import "./jquery-3.6.3.js";
 
 flsFunctions.isWebp();
 
@@ -15,12 +16,48 @@ document.addEventListener('DOMContentLoaded', () => {
             756: {
                 slidesPerView: 3,
             },
-
             320: {
                 slidesPerView: 1,
             }
             
         },
+    });
+
+    const swiper_2 = new Swiper('.house__items', {
+        modules: [Navigation, Pagination],
+        navigation: {
+            nextEl: ".house__arrow-right",
+            prevEl: ".house__arrow-left"
+        },
+        slidesPerView: 3.55,
+        loop: false,
+        breakpoints: {
+            1490: {
+                slidesPerView: 3.55,
+            },
+            1024: {
+                slidesPerView: 2.55,
+            },
+            865: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            500: {
+                centeredSlides: true,
+                slidesPerView: 2,
+            },
+            300: {
+                slidesPerView: 1,
+            },
+        }
+    });
+
+    $('ul.house__tabs').on('click', 'li:not(.house__tab_active)', function() {
+        $(this)
+          .addClass('house__tab_active').siblings().removeClass('house__tab_active')
+          .closest('div.container').find('div.house__items').removeClass('house__items_active').eq($(this).index()).addClass('house__items_active');
     });
 });
 
