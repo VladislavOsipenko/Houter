99f97809c9ecc9d4ec84cef8de0db16a215bbe53
Vladislav Osipenko <vladislavosipenko584@gmail.com>
2023-07-20 12:05:44 +0300

    Done all section with adaptation

--- src/js/app.js
+++ src/js/app.js
@@ -84,5 +84,54 @@ document.addEventListener('DOMContentLoaded', () => {
           .addClass('house__tab_active').siblings().removeClass('house__tab_active')
           .closest('div.container').find('div.house__items').removeClass('house__items_active').eq($(this).index()).addClass('house__items_active');
     });
+
+    const links = document.querySelectorAll('a');
+
+    function addPreventDefault(arg) {
+        arg.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                e.preventDefault();
+            })
+        })
+    }
+    addPreventDefault(links);
+
+    const animItems = document.querySelectorAll('._anim-items');
+
+    if(animItems.length > 0) {
+        window.addEventListener('scroll', animOnScroll);
+        function animOnScroll() {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart; 
+                }
+
+                if ((scrollY > animItemOffset - animItemPoint) && scrollY <(animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+                } else {
+                    if (!animItem.classList.contains('_anim-no-hide')) {
+                        animItem.classList.remove('_active');
+                    }
+                    
+                }
+            }
+        }
+        function offset(el) {
+            const rect = el.getBoundingClientRect(), 
+                scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+                scrollTop = window.scrollY || document.documentElement.scrollTop;
+            return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+        setTimeout(() => {
+            animOnScroll();
+        }, 400);
+    }
 });
 
